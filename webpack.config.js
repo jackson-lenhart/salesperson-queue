aea282637994f4083ef2d7591d2890fd3f3662a3
jackson-lenhart <jacksonlenhart@gmail.com>
2018-04-03 20:52:11 -0400

    Install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 "use strict";
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./main.js",
+  module: {
+      rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  devServer: {
+    contentBase: "./dist"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
