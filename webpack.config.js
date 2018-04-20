7a7d7a97e938799291c37735f8bf75374dd384dc
jackson-lenhart <jacksonlenhart@gmail.com>
2018-04-20 13:42:42 -0400

    Move client into folder aptly named

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./main.js",
+  entry: "./client/main.js",
   module: {
       rules: [
       {
